85c21651ba39187442bbb10bf59e012cc0616225
Talha Manzoor <talha.5023269@talmeez.pk>
2020-08-12 00:52:40 +0500

    updated

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Replace this with your MONGOURI.
+const MONGOURI = "mongodb+srv://Admin:12345@cluster0.a5xlk.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+const InitiateMongoServer = async () => {
+  try {
+    await mongoose.connect(MONGOURI, {
+      useNewUrlParser: true,useUnifiedTopology: true
+    });
+    console.log("Connected to DB !!");
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+module.exports = InitiateMongoServer;
